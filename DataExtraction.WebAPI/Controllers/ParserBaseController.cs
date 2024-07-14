a76cbd56d01b2c24cc86644d7409115a98f1ec01
somathedeveloper <bsomasekhar101@gmail.com>
2024-07-14 16:19:42 +0530

    Add project files.

---
+++ DataExtraction.WebAPI/Controllers/ParserBaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataExtraction.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParserBaseController : ControllerBase
+    {
+    }
+}
