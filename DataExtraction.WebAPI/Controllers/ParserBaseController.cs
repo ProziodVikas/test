f61e38ae916091911293f1635eb6cd4149eb331f
somathedeveloper <bsomasekhar101@gmail.com>
2024-07-14 19:10:18 +0530

    Tested using pdfpig library.

--- DataExtraction.WebAPI/Controllers/ParserBaseController.cs
+++ DataExtraction.WebAPI/Controllers/ParserBaseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DataExtraction.Library.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DataExtraction.WebAPI.Controllers
@@ -7,5 +8,36 @@ namespace DataExtraction.WebAPI.Controllers
     [ApiController]
     public class ParserBaseController : ControllerBase
     {
+        private readonly string billsFolderPath = "C:/InvoiceStore/IncomingBills";
+        private readonly IExtractPdfService _extractPdfService;
+
+        public ParserBaseController(IExtractPdfService extractPdfService) 
+        {
+            _extractPdfService = extractPdfService;
+        }
+
+        [HttpGet("BillParser")]
+        public async Task<ActionResult> BillParser()
+        {
+            if (!Directory.Exists(billsFolderPath))
+            {
+                return NotFound("Bills folder not found");
+            }
+            var files = Directory.GetFiles(billsFolderPath);
+            if (files.Length == 0)
+            {
+                return NotFound("Bills not found in the folder");
+            }
+            foreach(var filePath in files)
+            {
+                var extension = Path.GetExtension(filePath);
+                if (extension == ".pdf")
+                {
+                    var fileName = Path.GetFileName(filePath);
+                    var extractedText = await _extractPdfService.ExtractTextFromPdf(filePath);
+                }
+            }
+            return Ok();
+        }
     }
 }
---
+++ DataExtraction/Services/ExtractPdfService.cs
@@ -0,0 +1,36 @@
+﻿using DataExtraction.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
+using UglyToad.PdfPig.Util;
+
+namespace DataExtraction.Library.Services
+{
+    public class ExtractPdfService : IExtractPdfService
+    {
+        public async Task<List<string>> ExtractTextFromPdf(string filePath)
+        {
+            var extractedText = new List<string>();
+            byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+            using (var memoryStream = new MemoryStream(bytes))
+            {
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                using (var document = PdfDocument.Open(memoryStream))
+                {
+                    foreach(var page in document.GetPages())
+                    {
+                        extractedText.Add($"Page{page.Number}");
+                        var extractor = ContentOrderTextExtractor.GetText(page);
+                        var lines = extractor.Split("\r\n");
+                        extractedText.AddRange(lines);
+                    }
+                }
+            }
+            return extractedText;
+        }
+    }
+}
